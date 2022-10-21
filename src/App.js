6f1457159a939638029fa848b0a6f8cc891fbf62
nuthan <nuthanraj232@gmail.com>
2022-10-21 09:03:17 +0530

    git-users

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import User1 from './Gitusers/User1'
+import User2 from './Gitusers/User2'
+import User3 from './Gitusers/User3'
+import User4 from './Gitusers/User4'
+import User5 from './Gitusers/User5'
+import User6 from './Gitusers/User6'
+import User7 from './Gitusers/User7'
+import User8 from './Gitusers/User8'
+import User9 from './Gitusers/User9'
+import User10 from './Gitusers/User10'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='col-example'>
+    <User1 />
+    <User2 />
+    <User3 />
+    <User4 />
+    <User5 />
+    <User6 />
+    <User7 />
+    <User8 />
+    <User9 />
+    <User10 />
     </div>
-  );
+  )
 }
 
 export default App;
